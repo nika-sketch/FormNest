70db3cbfb81220231b2e600d2323ba1fedc32b89
Nikoloz Latsabidze <nilatsabidze@bog.ge>
2025-07-22 13:16:20 +0400

    added runCatching extension for cancellationException handling while using coroutines. modified viewModel for network call

--- app/src/main/java/com/example/formnest/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/formnest/presentation/MainViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.formnest.data.model.FormNestApiModel
 import com.example.formnest.di.NetworkModule
+import com.example.formnest.shared.runCatchingCancellable
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -21,11 +22,12 @@ class MainViewModel(
 
     init {
         viewModelScope.launch(Dispatchers.IO) {
-            try {
-                val result = networkModule.provideFormNestService().fetchSurveyData()
-                mutableState.value = result.body() ?: error("Body is null")
-            } catch (e: Exception) {
-                // TODO handle error and coroutine cancellation
+            runCatchingCancellable {
+                networkModule.provideFormNestService().fetchSurveyData()
+            }.onSuccess { response ->
+                mutableState.value = response.body() ?: error("Body is null")
+            }.onFailure { e ->
+                mutableState.value = FormNestApiModel(emptyList(), "", "")
             }
         }
     }
